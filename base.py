2169bacf165ecfdca09015c3df16bb84f6584e1f
luongtu996 <luongtu996@gmail.com>
2023-04-03 09:50:25 +0700

    feat: update logs

--- base.py
+++ base.py
@@ -7,7 +7,8 @@ class BasePage:
         # self.setup_method()
         # self.driver.implicitly_wait(10)
         # self.driver.set_window_size(1, 1)
-        self.logerror_path = './error_log.txt'
+        self.logerror_path = '.logs/error_log.txt'
+        self.loginfo_path = '.logs/info_log.txt'
         
     def setup_method(self):
         self.driver = webdriver.Chrome()
@@ -20,10 +21,19 @@ class BasePage:
     def teardown_method(self):
         self.driver.quit()
 
-    def writelog(self, logstring):
-        now = datetime.now()
-        current_time = now.strftime("%H:%M:%S")
+    def write_error_log(self, log_str):
+        self.writelog(log_str, 'error')
 
-        with open(self.logerror_path, 'a', encoding='utf8') as f:
+    def write_info_log(self, log_str):
+        self.writelog(log_str, 'info')
+
+    def writelog(self, logstring, type):
+        now = datetime.now()
+        current_time = now.strftime("%d%m%y ,%H:%M:%S")
+        log_path = self.loginfo_path
+        if(type == 'error'):
+            log_path = self.logerror_path
+
+        with open(log_path, 'a', encoding='utf8') as f:
             f.write(current_time + '\t' + logstring+'\n')
         f.close()
